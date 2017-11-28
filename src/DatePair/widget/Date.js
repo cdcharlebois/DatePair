e414c4ef0d2babe84e4b20e393c3182d06ee09f5
Conner Charlebois <cdcharlebois@gmail.com>
2017-11-28 10:07:23 -0500

    validation subscription

--- src/DatePair/widget/Date.js
+++ src/DatePair/widget/Date.js
@@ -7,124 +7,150 @@
  */
 
 define([
-    "dojo/_base/declare",
-    // "mxui/widget/_WidgetBase",
-    // "dijit/_TemplatedMixin",
-    // "mxui/dom",
-    // "dojo/dom",
-    // "dojo/dom-prop",
-    // "dojo/dom-geometry",
-    // "dojo/dom-class",
-    "dojo/dom-style",
-    // "dojo/dom-construct",
-    // "dojo/_base/array",
-    "dojo/_base/lang",
-    // "dojo/text",
-    // "dojo/html",
-    // "dojo/_base/event",
+        "dojo/_base/declare",
+        // "mxui/widget/_WidgetBase",
+        // "dijit/_TemplatedMixin",
+        // "mxui/dom",
+        // "dojo/dom",
+        // "dojo/dom-prop",
+        // "dojo/dom-geometry",
+        // "dojo/dom-class",
+        "dojo/dom-style",
+        // "dojo/dom-construct",
+        // "dojo/_base/array",
+        "dojo/_base/lang",
+        // "dojo/text",
+        // "dojo/html",
+        // "dojo/_base/event",
 
-    // external
-    "DatePair/lib/jquery-1.11.2",
-    "DatePair/lib/picker",
-    "DatePair/lib/picker.date",
-    "DatePair/lib/picker.time",
+        // external
+        "DatePair/lib/jquery-1.11.2",
+        "DatePair/lib/picker",
+        "DatePair/lib/picker.date",
+        "DatePair/lib/picker.time",
 
-    "dojo/text!DatePair/widget/template/DateTime.html",
-    "DatePair/widget/DateTime"
-  ],
-  function(declare,
-    // _WidgetBase,
-    // _TemplatedMixin,
-    // dom,
-    // dojoDom,
-    // dojoProp,
-    // dojoGeometry,
-    // dojoClass,
-    dojoStyle,
-    // dojoConstruct,
-    // dojoArray,
-    lang,
-    // dojoText,
-    // dojoHtml,
-    // dojoEvent,
-    _jQuery,
-    _picker,
-    _datepicker,
-    _timepicker,
-    widgetTemplate,
-    DateTimeWidget) {
-    "use strict";
+        "dojo/text!DatePair/widget/template/DateTime.html",
+        "DatePair/widget/DateTime"
+    ],
+    function(declare,
+        // _WidgetBase,
+        // _TemplatedMixin,
+        // dom,
+        // dojoDom,
+        // dojoProp,
+        // dojoGeometry,
+        // dojoClass,
+        dojoStyle,
+        // dojoConstruct,
+        // dojoArray,
+        lang,
+        // dojoText,
+        // dojoHtml,
+        // dojoEvent,
+        _jQuery,
+        _picker,
+        _datepicker,
+        _timepicker,
+        widgetTemplate,
+        DateTimeWidget) {
+        "use strict";
 
-    var $ = _jQuery.noConflict(true);
+        var $ = _jQuery.noConflict(true);
 
-    return declare("DatePair.widget.Date", [DateTimeWidget], {
+        return declare("DatePair.widget.Date", [DateTimeWidget], {
 
-      /**
-       * @override
-       */
-      postCreate: function() {
-        // hide the time node.
-        this.startTimeNode.style.display = "none";
-        if (!this.editable) {
-          this._setDisabled();
-        }
-        this.endDateNode.style.display = "none";
-        this.endTimeNode.style.display = "none";
-        this.toNode.style.display = "none";
-        this._addStyling();
-      },
+            /**
+             * @override
+             */
+            postCreate: function() {
+                // hide the time node.
+                this.startTimeNode.style.display = "none";
+                if (!this.editable) {
+                    this._setDisabled();
+                }
+                this.endDateNode.style.display = "none";
+                this.endTimeNode.style.display = "none";
+                this.toNode.style.display = "none";
+                this.errorNode.style.display = "none";
+                this._addStyling();
+            },
 
-      /**
-       * UPDATE
-       * ---
-       * @override
-       * init the timepicker and datepicker, set initial values
-       */
-      update: function(obj, callback) {
-        logger.debug(this.id + ".update");
-        this._contextObj = obj;
+            /**
+             * UPDATE
+             * ---
+             * @override
+             * init the timepicker and datepicker, set initial values
+             */
+            update: function(obj, callback) {
+                logger.debug(this.id + ".update");
+                this._contextObj = obj;
 
-        if (this._contextObj.isReadonlyAttr(this.fromDate)) {
-          this._setDisabled();
-        }
+                if (this._contextObj.isReadonlyAttr(this.fromDate)) {
+                    this._setDisabled();
+                }
 
-        this._getDatePickerOptions()
-          .then(lang.hitch(this, function(options) {
-            return new Promise(lang.hitch(this, function(resolve) {
-              this.dp = this._initDatepicker($(".date", this.domNode.firstElementChild), options);
-              resolve();
-            }));
-          }))
-          .then(lang.hitch(this, function() {
-            // set initial values
-            this._setDateTimePickerValues(this._contextObj.get(this.fromDate));
-            // set display
-            if (this.datePickerShouldStartOpen) {
-              this.dp.open();
+                this._getDatePickerOptions()
+                    .then(lang.hitch(this, function(options) {
+                        return new Promise(lang.hitch(this, function(resolve) {
+                            this.dp = this._initDatepicker($(".date", this.domNode.firstElementChild), options);
+                            resolve();
+                        }));
+                    }))
+                    .then(lang.hitch(this, function() {
+                        // set initial values
+                        this._setDateTimePickerValues(this._contextObj.get(this.fromDate));
+                        // set display
+                        if (this.datePickerShouldStartOpen) {
+                            this.dp.open();
+                        }
+                        // add handlers
+                        var _fromDateHandle = this.subscribe({
+                            guid: this._contextObj.getGuid(), // the guid
+                            attr: this.fromDate, // the attributeName
+                            callback: lang.hitch(this, function(guid, attr, attrValue) {
+                                this._clearValidation();
+                                this._setDateTimePickerValues(attrValue);
+                            })
+                        });
+                        var _objHandle = this.subscribe({
+                            guid: this._contextObj.getGuid(), // the guid
+                            callback: lang.hitch(this, function(guid, attr, attrValue) {
+                                this._clearValidation();
+                                this._setDateTimePickerValues(attrValue);
+                            })
+                        });
+                        var _valHandle = this.subscribe({
+                            guid: this._contextObj.getGuid(),
+                            val: true,
+                            callback: lang.hitch(this, function(validations) {
+                                this._handleValidations(validations);
+                            })
+                        })
+                        this._handles.concat([_fromDateHandle, _objHandle, _valHandle]);
+                        this._updateRendering(callback);
+                    }))
+                    .catch(lang.hitch(this, function(err) {
+                        console.error(err);
+                        this._updateRendering(callback);
+                    }));
+            },
+
+            _handleValidations: function(validations) {
+                var validation = validations[0],
+                    message = validation.getReasonByAttribute(this.fromDate);
+                this._showValidation(message); // update widget DOM
+            },
+
+            _showValidation: function(message) {
+                this.errorNode.innerHTML = message;
+                this.errorNode.style.display = "block";
+            },
+
+            _clearValidation: function() {
+                this.errorNode.style.display = "none";
             }
-            // add handlers
-            var _fromDateHandle = this.subscribe({
-              guid: this._contextObj.getGuid(), // the guid
-              attr: this.fromDate, // the attributeName
-              callback: lang.hitch(this, function(guid, attr, attrValue) {
-                this._setDateTimePickerValues(attrValue);
-              })
-            });
-            var _objHandle = this.subscribe({
-              guid: this._contextObj.getGuid(), // the guid
-              callback: lang.hitch(this, function(guid, attr, attrValue) {
-                this._setDateTimePickerValues(attrValue);
-              })
-            });
-            this._handles.concat([_fromDateHandle, _objHandle]);
-            this._updateRendering(callback);
-          }))
-          .catch(lang.hitch(this, function(err) {
-            console.error(err);
-            this._updateRendering(callback);
-          }));
-      },
+
+        });
     });
-  });
 
 require(["DatePair/widget/Date"]);
